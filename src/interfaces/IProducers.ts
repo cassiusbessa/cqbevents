6599b19c36354e4a1416f5553505c599d29ddff6
cassiusbessa <cassiusbessa@gmail.com>
2022-11-24 04:17:28 -0300

    feat(Interfaces): :art: implements Producers interface

---
+++ src/interfaces/IProducers.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const IProducerZodSchema = z.object({
+  email: z.string({ required_error: 'email is required',
+    invalid_type_error: 'email must be a string' })
+    .email({ message: 'email must be a valid email' }),
+  username: z.string({ required_error: 'username is required',
+    invalid_type_error: 'username must be a string' })
+    .min(3, { message: 'username must be at least 3 characters' }),
+  password: z.string({ required_error: 'password is required',
+    invalid_type_error: 'password must be a string' })
+    .min(6, { message: 'password must be at least 6 characters' }),
+});
+
+export type IProducer = z.infer<typeof IProducerZodSchema>;
