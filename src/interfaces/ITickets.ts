7aff33784ad1fc8396c53083b8a5842c3e682944
cassiusbessa <cassiusbessa@gmail.com>
2022-11-24 05:01:45 -0300

    feat(Interfaces): :art: implements Tickets interface

---
+++ src/interfaces/ITickets.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const ITicketsZodSchema = z.object({
+  title: z.string({ required_error: 'title is required',
+    invalid_type_error: 'title must be a string' })
+    .min(3, { message: 'title must be at least 3 characters' }),
+  startDate: z.date({ required_error: 'startDate is required',
+    invalid_type_error: 'startDate must be a date' }),
+  endDate: z.date({ required_error: 'endDate is required',
+    invalid_type_error: 'endDate must be a date' }),
+  quantity: z.number({ required_error: 'quantity is required',
+    invalid_type_error: 'quantity must be a number' }),
+  solds: z.number({ required_error: 'solds is required',
+    invalid_type_error: 'solds must be a number' }),
+  price: z.number({ required_error: 'price is required',
+    invalid_type_error: 'price must be a number' }),
+  description: z.string().optional(),
+});
+
+export type ITickets = z.infer<typeof ITicketsZodSchema>;
