c6b5c23e66e110bb2be3052542842154142e8a7a
cassiusbessa <cassiusbessa@gmail.com>
2022-11-25 06:05:54 -0300

    feat: :sparkles: implements basecontroller to a basic crud

---
+++ src/controllers/BaseController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { httpStatusCode, CtrlWrapper } from '../utils';
+import { BaseCases } from '../useCases';
+
+export default class BaseController<T, U> {
+  constructor(private useCase: BaseCases<T, U>) {
+    this.useCase = useCase;
+  }
+
+  public create = CtrlWrapper(async (req: Request, res: Response) => {
+    const { body } = req;
+    const result = await this.useCase.create(body);
+    return res.status(httpStatusCode.CREATED).json(result);
+  });
+
+  public read = CtrlWrapper(async (_req: Request, res: Response) => {
+    const result = await this.useCase.read();
+    return res.status(httpStatusCode.OK).json(result);
+  });
+
+  public readOne = CtrlWrapper(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await this.useCase.readOne(id);
+    return res.status(httpStatusCode.OK).json(result);
+  });
+
+  public update = CtrlWrapper(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { body } = req;
+    const result = await this.useCase.update(id, body);
+    return res.status(httpStatusCode.OK).json(result);
+  });
+
+  public delete = CtrlWrapper(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await this.useCase.delete(id);
+    return res.status(httpStatusCode.OK).json(result);
+  });
+}
---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+import BaseController from './BaseController';
+
+// eslint-disable-next-line import/prefer-default-export
+export { BaseController };
