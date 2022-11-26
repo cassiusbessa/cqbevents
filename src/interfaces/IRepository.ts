c78aa709eeb104834e60cf6fb1de85078ec4f71e
cassiusbessa <cassiusbessa@gmail.com>
2022-11-26 19:19:54 -0300

    refactor(Interfaces): :art: reads methods now is most generic

--- src/interfaces/IRepository.ts
+++ src/interfaces/IRepository.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 interface IRepository<T, U> {
   create(obj:T):Promise<T>,
-  read():Promise<Array<T>>,
-  readOne(_id:string):Promise<T | null>,
+  read(where: any):Promise<Array<T>>,
+  readOne(where: any):Promise<T | null>,
   update(_id: string, obj:U):Promise<T | null>,
-  delete(_id:string):Promise<T | null>
+  delete(_id:string):Promise<T | null>,
+  find(username: string, email: string): Promise<T | null>
 }
 
 export default IRepository;
