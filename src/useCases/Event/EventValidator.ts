ee14937e0e571e9c1e6f84c04ea0d9ec4560eee9
cassiusbessa <cassiusbessa@gmail.com>
2022-11-28 04:47:22 -0300

    feat(UseCases): :sparkles: implements validate with relation between sales and tickets date

--- src/useCases/Event/EventValidator.ts
+++ src/useCases/Event/EventValidator.ts
@@ -1,44 +1,65 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable class-methods-use-this */
 import { Validator } from '..';
-import { IEvent, IEventUpdate, IEventZodSchema, IEventZodSchemaUpdate } from '../../interfaces';
+import { IEvent, IEventUpdate, IEventZodSchema, IEventZodSchemaUpdate, 
+  ITicket } from '../../interfaces';
 
 export default class EventValidator extends Validator<IEvent, IEventUpdate> {
   constructor() {
     super(IEventZodSchema, IEventZodSchemaUpdate);
   }
 
-  public dateValidate(event: IEvent) {
-    console.log('chamei validador');
+  private stardAndEndDate(event: IEvent) {
     const { attractions, tickets } = event;
-    console.log(tickets[0].startDate, tickets[0].endDate);
     attractions.forEach((attraction) => {
       if (attraction.startDate >= attraction.endDate) {
         throw new Error('Attraction Start date must be before end date');
       }
     });
-    tickets.forEach((ticket) => {
+    tickets.forEach((ticket: ITicket) => {
       if (ticket.startDate >= ticket.endDate) {
         throw new Error('Sale Start date must be before end date');
       }
     });
   }
 
-  public async startSaleDate(event: IEvent) {
-    const firstSaleDate = event.tickets.reduce((acc: any, ticket: any) => {
+  private firstSaleDate(event: IEvent) {
+    return event.tickets.reduce((acc: any, ticket: any) => {
       if (ticket.startDate < acc) {
         return ticket.startDate;
       }
       return acc;
     });
-    const firstAtractionDate = event.attractions.reduce((acc: any, attraction: any) => {
+  }
+
+  private firstAtractionDate(event: IEvent) {
+    return event.attractions.reduce((acc: any, attraction: any) => {
       if (attraction.startDate < acc) {
         return attraction.startDate;
       }
       return acc;
     });
+  }
+
+  private saleAndAttractionDate(event: IEvent) {
+    const firstSaleDate = this.firstSaleDate(event) as any;
+    console.log(firstSaleDate);
+    const firstAtractionDate = this.firstAtractionDate(event) as any;
     if (firstSaleDate > firstAtractionDate) {
       throw new Error('Sale Start date must be before first attraction date');
     }
+
+    if (firstSaleDate.startDate < Date.now()) {
+      throw new Error('Sale Start date must be after now');
+    }
+
+    if (firstAtractionDate.startDate < Date.now()) {
+      throw new Error('Attraction Start date must be after now');
+    }
+  }
+
+  public dataValidate(event: IEvent) {
+    this.stardAndEndDate(event);
+    this.saleAndAttractionDate(event);
   }
 } 
